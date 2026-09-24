66dbdbd6224884c3a537f3ffe9a4f943007b9380
Mateo Vignoli <mateunlpe@gmail.com>
2026-09-24 14:35:47 -0300

    estructura consigna dos

--- EntregaPractica.py
+++ EntregaPractica.py
@@ -1,4 +1,4 @@
-columnas_eph = {
+columnas = {
     "PONDERA": {"tipo": "int", "completitud": 100},
     "ESTADO": {"tipo": "int", "completitud": 95},
     "CAT_OCUP": {"tipo": "int", "completitud": 80},
@@ -10,4 +10,25 @@ columnas_eph = {
     "ITF": {"tipo": "int", "completitud": 60},
     "MAS_500": {"tipo": "str", "completitud": 100},
     "GDECCFR": {"tipo": "int", "completitud": 55}
+}
+
+# 
+
+roles = {
+    "docente": {
+        "columnas": ["EDAD", "ESTADO", "REGION", "AGLOMERADO"],
+        "criterio": "nombre",
+        "orden": "A"
+    },
+    "investigador": {
+        "columnas": ["PONDERA", "ESTADO", "CAT_OCUP", "ITF", "GDECCFR"],
+        "criterio": "completitud",
+        "orden": "B",
+        "umbral": 80
+    },
+    "analista": {
+        "columnas": ["REGION", "AGLOMERADO", "MAS_500", "ANO4", "TRIMESTRE"],
+        "criterio": "completitud",
+        "orden": "A"
+    }
 }
