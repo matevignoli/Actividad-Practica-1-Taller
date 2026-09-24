f876bd6241e91dc2730ca1170b16b9e41d6db13e
Mateo Vignoli <mateunlpe@gmail.com>
2026-09-23 22:33:38 -0300

    consigna 1

---
+++ EntregaPractica.py
@@ -0,0 +1,13 @@
+columnas_eph = {
+    "PONDERA": {"tipo": "int", "completitud": 100},
+    "ESTADO": {"tipo": "int", "completitud": 95},
+    "CAT_OCUP": {"tipo": "int", "completitud": 80},
+    "EDAD": {"tipo": "int", "completitud": 99},
+    "REGION": {"tipo": "int", "completitud": 100},
+    "AGLOMERADO": {"tipo": "int", "completitud": 100},
+    "ANO4": {"tipo": "int", "completitud": 100},
+    "TRIMESTRE": {"tipo": "int", "completitud": 100},
+    "ITF": {"tipo": "int", "completitud": 60},
+    "MAS_500": {"tipo": "str", "completitud": 100},
+    "GDECCFR": {"tipo": "int", "completitud": 55}
+}
